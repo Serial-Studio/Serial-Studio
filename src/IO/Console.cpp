fda3b6b11e3ea1ce07c1394e754fe4789ed3f098
Alex Spataru <alex_spataru@outlook.com>
2021-02-05 17:57:28 -0500

    Fix console behaviour on Windows

--- src/IO/Console.cpp
+++ src/IO/Console.cpp
@@ -416,7 +416,7 @@ void Console::setDisplayMode(const DisplayMode mode)
  * is set to @c true, an timestamp is added for each line.
  */
 void Console::append(const QString &string, const bool addTimestamp)
-{
+{	
     // Get current date
     QString timestamp;
     if (addTimestamp)
@@ -445,7 +445,7 @@ void Console::append(const QString &string, const bool addTimestamp)
             m_timestampAdded = true;
         }
 
-        if (string.at(i) == "\n" || string.at(i) == "\r")
+        if (data.at(i) == "\n" || data.at(i) == "\r")
         {
             m_data.append("");
             m_timestampAdded = false;
@@ -458,7 +458,7 @@ void Console::append(const QString &string, const bool addTimestamp)
             m_data.replace(lineCount() - 1, str);
         }
     }
-
+	
     // Remove extra lines
     while (lineCount() > SCROLLBACK)
     {
