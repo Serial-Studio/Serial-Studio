b52f490c57754217b8834d3eafefb7475045cfeb
Alex Spataru <alex_spataru@outlook.com>
2021-12-07 01:10:16 -0600

    Update appcast & set development version to 1.1.5

--- src/AppInfo.h
+++ src/AppInfo.h
@@ -23,7 +23,7 @@
 #pragma once
 
 // clang-format off
-#define APP_VERSION     "1.1.4"
+#define APP_VERSION     "1.1.5"
 #define APP_DEVELOPER   "Alex Spataru"
 #define APP_NAME        "Serial Studio"
 #define APP_ICON        ":/images/icon.svg"
