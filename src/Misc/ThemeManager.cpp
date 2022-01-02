888f8d3d8fa123db964ce286bac8e851accdc5db
Alex Spataru <alex_spataru@outlook.com>
2022-01-02 12:57:26 -0500

    Disable custom window for Qt < 5.12

--- src/Misc/ThemeManager.cpp
+++ src/Misc/ThemeManager.cpp
@@ -41,7 +41,12 @@ Misc::ThemeManager::ThemeManager()
 {
     populateThemes();
     loadTheme(m_settings.value("themeId", 0).toInt());
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 12, 0)
+    setCustomWindowDecorations(m_settings.value("customWindows", false).toBool());
+#else
     setCustomWindowDecorations(m_settings.value("customWindows", true).toBool());
+#endif
 }
 
 /**
