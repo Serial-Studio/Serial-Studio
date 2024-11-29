2fb1f733cb4f493772a22090a4ae85c55b3afab1
Bernd Donner <bernd.donner@sabel.com>
2024-11-29 18:05:31 +0100

    Serial Studio is starting up on nixOS. Functions mostly untested. NixOS 24.11 for qt 6.8 library

--- flake.nix
+++ flake.nix
@@ -11,10 +11,10 @@
 # 
 
 {
-  description = "C++ Development with Nix 24.05";
+  description = "C++ Development with Nix 24.11";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
